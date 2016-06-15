7e6d0c65f4529f3327fe2159ad6476824ab948dd
George Chung <Gerhut@GMail.com>
2016-06-15 09:52:42 +0800

    fix rss format to meet IFTTT requirements

--- index.js
+++ index.js
@@ -7,10 +7,23 @@ const Feed = require('feed')
 
 const packageInfo = require('./package')
 
-const getRecentTag = (repo) => superagent
-  .get(`https://api.github.com/repos/${repo}/tags`)
-  .query({ per_page: 1 })
-  .then((res) => Object.assign(res.body[0], { repo }))
+const getRecentTag = (repo) => {
+  const tag = { repo }
+  return superagent
+    .get(`https://api.github.com/repos/${repo}/tags`)
+    .query({ per_page: 1 })
+    .then((res) => {
+      const tagInfo = res.body[0]
+      tag.name = tagInfo.name
+      tag.commit = tagInfo.commit.sha
+      return superagent.get(tagInfo.commit.url)
+    })
+    .then((res) => {
+      const commit = res.body.commit
+      tag.date = new Date(Date.parse(commit.committer.date))
+      return tag
+    })
+}
 
 const ReleasesTracker = module.exports = (repos) => (req, res) => {
   Promise.all(repos.map(getRecentTag)).then((tags) => {
@@ -22,12 +35,18 @@ const ReleasesTracker = module.exports = (repos) => (req, res) => {
     tags.forEach((tag) => feed.addItem({
       title: `${tag.repo} ${tag.name} released`,
       link: `https://github.com/${tag.repo}/releases/tag/${tag.name}`,
-      guid: `https://github.com/${tag.repo}/commit/${tag.commit.sha}`
+      guid: `https://github.com/${tag.repo}/commit/${tag.commit.sha}`,
+      date: tag.date
     }))
     res.writeHead(200, {
       'Content-Type': 'application/rss+xml'
     })
     res.end(feed.render())
+  }).catch((err) => {
+    res.writeHead(500, err.message, {
+      'Content-Type': 'text/plain'
+    })
+    res.end(err.stack)
   })
 }
 
--- test.js
+++ test.js
@@ -3,32 +3,38 @@
 /* eslint-env mocha */
 
 const supertest = require('supertest')
-const FeedParser = require('feedparser')
+const htmlparser = require('htmlparser')
 
 const ReleasesTracker = require('.')
 
-it('should work', function (done) {
-  this.timeout(10000)
+it('should generate RSS feed meet the requirements of IFTTT Feed Channel',
+  function (done) {
+    this.timeout(10000)
 
-  const middleware = ReleasesTracker([
-    'jquery/jquery',
-    'twbs/bootstrap'
-  ])
+    const middleware = ReleasesTracker([
+      'jquery/jquery',
+      'twbs/bootstrap'
+    ])
 
-  supertest(middleware)
-    .get('/')
-    .expect(200)
-    .expect('Content-Type', 'application/rss+xml')
-    .expect((res) => {
-      const feedparser = new FeedParser()
-      res.pipe(feedparser)
-      feedparser.on('error', done)
-      feedparser.on('readable', () => {
-        let item
-        while ((item = feedparser.read())) {
-          item.title.should.be.a.String().and.match(/jquery\/jquery|twbs\/bootstrap/)
-        }
+    supertest(middleware)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'application/rss+xml')
+      .end((err, res) => {
+        if (err) throw err
+
+        const handler = new htmlparser.RssHandler((err, feed) => {
+          if (err) throw err
+
+          feed.should.have.properties('title', 'link')
+          feed.items.forEach((item) => {
+            item.should.have.properties('title', 'id', 'pubDate')
+            item.title.should.be.a.String().and.match(/jquery\/jquery|twbs\/bootstrap/)
+            item.pubDate.should.be.a.Date()
+          })
+          done()
+        })
+        const parser = new htmlparser.Parser(handler)
+        parser.parseComplete(res.text)
       })
-    })
-    .end(done)
-})
+  })
