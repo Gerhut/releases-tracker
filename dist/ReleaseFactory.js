aff8cf845aeb701274e714f681851e64cab0c285
George Chung <Gerhut@GMail.com>
2018-11-27 11:08:52 +0800

    Update dependencies

--- dist/ReleaseFactory.js
+++ dist/ReleaseFactory.js
@@ -1,7 +1,7 @@
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
-}
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const url_1 = require("url");
 const GitHubRelease_1 = __importDefault(require("./releases/GitHubRelease"));
